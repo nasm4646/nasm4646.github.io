c7f8f25caed1539472c4202fba5ea7a5e563760a
Tasha Smith <nasm4646@colorado.edu>
2023-03-26 22:13:18 -0600

    wa10

---
+++ wa/wa10/main.js
@@ -0,0 +1,52 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+
+
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : 'Pink gummy bear earrings',
+  'pic2.jpg' : 'Assorted gummy bear single earrings',
+  'pic3.jpg' : 'Assorted gummy bear earrings in pairs',
+  'pic4.jpg' : 'Far away shot of gummy bear earring pairs',
+  'pic5.jpg' : 'Pink gummy bear earrings with black background'
+}
+
+
+/* Looping through images */
+/* In each loop iteration, replace the xxx placeholder values with a 
+string that will equal the path to the image and alt attributes in each case. */
+
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  /* add a click event listener to the current newImage */
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } 
+  else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
