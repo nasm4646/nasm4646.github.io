66255e3242f6e1f35741ee4ca262227d41083e35
Tasha Smith <nasm4646@colorado.edu>
2023-05-06 13:13:20 -0600

    Update script.js

--- js/jsproject/script.js
+++ js/jsproject/script.js
@@ -1,4 +1,4 @@
-//currently at 10:11 in video
+//currently at  in video
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
@@ -30,12 +30,74 @@ window.addEventListener('load', function(){
     }
 
     class Player {
-
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 200;
+            this.height = 200;
+            this.x = 10;
+            this.y = this.gameHeight - this.height;
+            this.image = document.getElementById('playerImage');
+            this.frameX = 0;
+            this.framey = 0;
+            this.speed = 0;
+            this.vy = 0;
+        }
+        draw(context){
+            context.fillStyle = 'white';
+            context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height,
+            this.width, this.height, this.x, this.y, this.width, this,height);
+        }
+        update(input){
+            if (input.keys.indexOf('ArrowRight') > -1){
+                this.speed = 5;
+            } else if (input.keys.indexOf('ArrowLeft') > -1) {
+                this.speed = -5;
+            } else if (input.keys.indexOf('ArrowUp') > -1) {
+                this.vy -= -32;
+            } else {
+                this.speed = 0;
+            }
+            // horizontal movement
+            this.x += this.speed;
+            if (this.x < 0) this.x = 0;
+            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+            // vertical movement
+            this.y += this.vy;
+            if (!this.onGround()){
+                this.vy += this.weight;
+                this.frameY = 1;
+            } else {
+                this.vy = 0;
+                this.frameY = 0;
+            }
+            if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+        }
+        onGround(){
+            return this.y >= this.gameHeight - this.height;
+        }
     }
 
     class Background {
-
-
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById('backgroundImage');
+            this.x = 0;
+            this.y = 0;
+            this.wdith = 2400;
+            this.height = 720;
+            this.speed = 7;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height);
+        }
+        update(){
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) this.x=0;
+        }
     }
 
     class Enemy {
@@ -51,8 +113,16 @@ window.addEventListener('load', function(){
     }
 
     const input = new InputHandler();
-
+    const player = new Player(canvas.width, canvas.height);
+    const background = new Background(canvas.wdith, canvas.height);
+    
     function animate(){
-
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        background.draw(ctx);
+        //background.update();
+        player.draw(ctx);
+        player.update(input);
+        requestAnimationFrame(animate);
     }
+    animate();
 });
