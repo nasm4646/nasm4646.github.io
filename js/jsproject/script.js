b724c4dc4f41203e1654713e1e097c8ed2567cd0
Tasha Smith <nasm4646@colorado.edu>
2023-05-05 20:27:09 -0600

    prj3

---
+++ js/jsproject/script.js
@@ -0,0 +1,58 @@
+//currently at 10:11 in video
+
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+
+    class InputHandler {
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                if ((   e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight')
+                        && this.keys.indexOf(e.key) === -1){
+                    this.keys.push(e.key);
+                }
+            });
+            window.addEventListener('keyup', e => {
+                if (    e.key === 'ArrowDown' ||
+                        e.key === 'ArrowUp' ||
+                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowRight'){
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+            });
+        }
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies(){
+
+    }
+
+    function displayStatusText(){
+
+    }
+
+    const input = new InputHandler();
+
+    function animate(){
+
+    }
+});
